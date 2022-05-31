439ec094e4491a6faf9b805558f01af317668e6a
Hundertmarck Jörg, HU <hundertmarck@boehme-weihs.de>
2022-05-31 12:58:00 +0200

    stderr

--- src/app.service.ts
+++ src/app.service.ts
@@ -14,27 +14,29 @@ export class AppService {
     return this.process != null;
   }
 
+  private onData(data: string): void {
+    this.stdout += data;
+
+    if (this.stdout.endsWith('Password for VPN:')) {
+      this.process.stdin.write(this.password);
+    }
+    if (this.stdout.endsWith('(Y/N))')) {
+      this.process.stdin.write('Y');
+    }
+    if (this.stdout.endsWith('A FortiToken code is required for SSL-VPN login authentication.')) {
+      this.process.stdin.write('Y');
+    }
+
+    // Errors
+    // 'NOTICE::Insufficient credential(s). Please check the password, client certificate, etc.'
+  }
+
   start(): void {
     this.stdout = '';
     this.process = spawn('/opt/forticlientsslvpn/64bit/forticlientsslvpn_cli', ['--server', this.ipAndPort, '--user', this.user]);
 
-    this.process.stdout.on('data', (data) => {
-      this.stdout += data;
-
-      if (this.stdout.endsWith('Password for VPN:')) {
-        this.process.stdin.write(this.password);
-      }
-      if (this.stdout.endsWith('(Y/N))')) {
-        this.process.stdin.write('Y');
-      }
-      if (this.stdout.endsWith('A FortiToken code is required for SSL-VPN login authentication.')) {
-        this.process.stdin.write('Y');
-      }
-
-      // Errors
-      // 'NOTICE::Insufficient credential(s). Please check the password, client certificate, etc.'
-      
-    });
+    this.process.stdout.on('data', this.onData);
+    this.process.stderr.on('data', this.onData);
 
     this.process.on('exit', () => {
       console.log('\n***ENDE');
