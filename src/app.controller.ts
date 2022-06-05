54741628bda7d402beb81143d30a5a16beae7602
Hundertmarck Jörg, HU <hundertmarck@boehme-weihs.de>
2022-06-05 15:14:11 +0200

    wip

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,24 @@
-import { Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { ApiProperty } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
 export interface StatusResult {
   error: boolean;
   message: string;
+}
+
+export class StartRequest {
+  @ApiProperty()
+  ipAndPort: string;
+
+  @ApiProperty()
+  user: string;
+
+  @ApiProperty()
+  password: string;
+
+  @ApiProperty()
+  otp: string;
 }
 
 @Controller('/api/vpn')
@@ -32,18 +47,19 @@ export class AppController {
 
   @Post('/start')
   start(
-    @Param('ipAndPort') ipAndPort: string,
-    @Param('user') user: string,
-    @Param('password') password: string,
-    @Param('otp') otp: string,
+    // @Param('ipAndPort') ipAndPort: string,
+    // @Param('user') user: string,
+    // @Param('password') password: string,
+    // @Param('otp') otp: string,
+    @Body() startRequest: StartRequest
   ): void {
-    this.appService.ipAndPort = ipAndPort;
-    this.appService.user = user;
-    this.appService.password = password;
-    this.appService.otp = otp;
+    this.appService.ipAndPort = startRequest.ipAndPort;
+    this.appService.user = startRequest.user;
+    this.appService.password = startRequest.password;
+    this.appService.otp = startRequest.otp;
 
-    console.log(`ipAndPort: ${ipAndPort}`);
-    console.log(`user: ${user}`);
+    console.log(`ipAndPort: ${startRequest.ipAndPort}`);
+    console.log(`user: ${startRequest.user}`);
 
     this.appService.start();
   }
